6213efd545849413f3fbb9b486c03f975d1ed104
Aadhar Kulshrestha <aadharkulshrestha3150@gmail.com>
2025-12-15 14:03:51 +0530

    Add Windows runner support with console output and DPI awareness
    - Implemented console creation and output redirection in utils.cpp.
    - Added functions to convert command line arguments from UTF-16 to UTF-8.
    - Created a manifest file for the Windows runner to enable DPI awareness.
    - Developed Win32Window class to manage window creation, destruction, and theme updates.
    - Integrated dark mode support based on system preferences.
    - Enhanced window handling with DPI scaling and message routing.

---
+++ USER/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+ 
+
+  runApp(const MyApp());
+}
+
+final supabase = Supabase.instance.client;
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'USER App',
+     
+    );
+  }
+}
+
